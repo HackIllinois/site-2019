1dadd503b9f83b78d3479a1c8337f374b9ac025b
Jonathan Xu <jonathanj.xu97@gmail.com>
2018-06-27 17:47:05 -0700

    Responsiveness

--- source/scenes/Home/index.jsx
+++ source/scenes/Home/index.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import logo from 'assets/Logo.svg';
 import diver from 'assets/diver.png';
+import diverMobile from 'assets/diver_mobile.png';
 
 import './styles.scss';
 
 const Home = () => (
   <div className="home">
-    <img className="diver" src={diver} alt="Diver Graphic" />
+    <img className="diver diver-desktop" src={diver} alt="Diver Graphic" />
+    <img className="diver diver-mobile" src={diverMobile} alt="Diver Graphic" />
+
     <div className="info-text">
       <img className="logo" src={logo} alt="HackIllinois Logo" />
       <h1>Dive Deeper</h1>
