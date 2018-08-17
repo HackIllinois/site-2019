b0c929d4c0b35993daaa03de4e7bcaba95d3349f
Jonathan Xu <jonathanj.xu97@gmail.com>
2018-08-16 19:30:29 -0700

    Mentorship documented added, used new generic PDF viewer

--- source/App.jsx
+++ source/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Error from 'scenes/Error';
 import Home from 'scenes/Home';
-import Sponsor from 'scenes/Sponsor';
+import PDFView from 'scenes/PDFView';
 
 import './styles.scss';
 
@@ -11,7 +11,8 @@ const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route path="/sponsor" component={Sponsor} />
+      <Route exact path="/sponsor" component={PDFView('/assets/sponsorship-2019.pdf')} />
+      <Route exact path="/mentor" component={PDFView('/assets/mentorship-2019.pdf')} />
       <Route component={() => <Error message="404 Not Found" />} />
     </Switch>
   </BrowserRouter>
