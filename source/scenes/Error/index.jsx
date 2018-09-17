a9b273d8fdc87c01e5c04cb0e6096c650b75508e
Pavan Baloo <Pavan Baloo>
2018-09-17 13:36:53 -0500

    recreated error page, added asset for 404 icon

--- source/scenes/Error/index.jsx
+++ source/scenes/Error/index.jsx
@@ -2,18 +2,19 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import Logo from 'assets/Logo.svg';
-
+import Error from 'assets/Error.png';
 import Footer from 'components/Footer';
 
 import './styles.scss';
 
 const ErrorPage = props => {
+  message = 'Pavan Baloo made this';
   const { message } = props;
   return (
     <div className="error">
       <div className="content-wrapper">
         <div className="contents">
-          <img src={Logo} alt="HackIllinois Logo" />
+          <img src={Error} alt="404 Error" />
           <h1>{message}</h1>
         </div>
         <Footer />
