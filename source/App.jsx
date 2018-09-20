d418a938da57a9271952d50100a5fdefbb37f467
Brian Yang <brian.yang328@gmail.com>
2018-09-19 21:06:31 -0500

    Undo changes

--- source/App.jsx
+++ source/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Error from 'scenes/Error';
 import Home from 'scenes/Home';
 import Sponsor from 'scenes/Sponsor';
-import InternalBrian from 'scenes/InternalBrian';
 import KrzyszD from 'scenes/KrzyszD';
 
 import './styles.scss';
@@ -14,7 +13,6 @@ const App = () => (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/sponsor" component={Sponsor} />
-      <Route path="/404" component={InternalBrian} />
       <Route path="/krzyszd" component={KrzyszD} />
       <Route component={() => <Error message="404 Not Found" />} />
     </Switch>
