e311158eacb54edb981be6b3e5a5212599cb1e5d
joshuakim0724 <joshuakim0724@gmail.com>
2018-09-19 21:33:34 -0500

    Added Josh page

---
+++ source/scenes/Josh/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Logo from 'assets/Logo.svg';
+
+import Footer from 'components/Footer';
+
+import './styles.scss';
+
+const Josh = () => (
+  <div className="error">
+    <div className="content-wrapper">
+      <div className="contents">
+        <img src={Logo} alt="HackIllinois Logo" />
+        <h1>{Josh}</h1>
+      </div>
+      <Footer />
+    </div>
+  </div>
+);
+
+export default Josh;
