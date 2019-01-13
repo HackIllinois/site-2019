78c7784683601cc27b777c2803417c737e488451
Justin Yang <justin@y4ng.com>
2019-01-12 23:57:06 -0600

    Update FAQ for 2019

--- source/scenes/Home/components/FAQ/Before.jsx
+++ source/scenes/Home/components/FAQ/Before.jsx
@@ -6,33 +6,36 @@ const Before = () => (
       <section className="faq-column">
         <h4>What should I bring?</h4>
         <p>
-          You should bring a student ID, a change of clothing, personal items (toiletries), a laptop
-          &amp; charger, and any required hardware for your hack. Due to power concerns, please do
-          not bring extra monitors.
+          You should bring a student ID, a reusable water bottle, a change of clothing, personal
+          items such as toiletries, and a laptop &amp; charger. Due to safety considerations, please
+          do not bring desktop computers, extra monitors, firearms, or alcoholic beverages.
         </p>
       </section>
       <section className="faq-column">
         <h4>When will I find out if I can attend?</h4>
         <p>
           Decisions will be released one month ahead of the event in January to allow time for
-          hackers to make transportation plans. RSVP is required to check in, so be on the lookout
+          students to make transportation plans. RSVP is required to check in, so be on the lookout
           for your acceptance email!
         </p>
       </section>
       <section className="faq-column">
         <h4>How do I get there?</h4>
         <p>
-          We will be providing buses from across the Midwest. Last year, we had buses from schools
-          spanning the states of Illinois, Iowa, Wisconsin, Missouri, Indiana, and Ohio. Travel
-          reimbursement may be provided in a limited form.
+          We will be providing buses to certain locations across the Midwest. Last year,
+          HackIllinois had <a href="https://2018.hackillinois.org/travel">these bus routes</a>{' '}
+          (subject to change this year). HackIllinois will not be providing travel reimbursements to
+          students.
         </p>
       </section>
       <section className="faq-column">
         <h4>Do I need a team?</h4>
         <p>
-          You can work with up as many people as you want! Single person and pair teams are allowed,
-          but we encourage you to work in a group. If you wish to form a team at HackIllinois, there
-          will be events to pair potential team members.
+          Due to the nature of HackIllinois as an open source event, you are encouraged to work with
+          others to contribute to open source projects. In particular, you are required to work with
+          a mentor in order to be eligible for a prize. Mentors will be available on Slack before
+          the event, as well at a project session on Friday. There are no team size limits, but most
+          contributions end up coming from teams of around 6-8.
         </p>
       </section>
     </div>
--- source/scenes/Home/components/FAQ/General.jsx
+++ source/scenes/Home/components/FAQ/General.jsx
@@ -7,35 +7,35 @@ const General = () => (
         <h4>What</h4>
         <p>
           HackIllinois is a 36-hour event where college students from across the country work on
-          Open Source Software projects ranging from websites and mobile apps to Wi-Fi connected
-          microwaves. You can work on creating a useful tool that you wish existed or learning a new
-          skill or language - anything goes!
+          open source projects, including new features, documentation, and bug fixes. You can work
+          on contributing to a useful open source tool that you use, a open source project of your
+          own, or joining a team led by an open source mentor.
         </p>
       </section>
       <section className="faq-column">
         <h4>Where</h4>
         <p>
-          HackIllinois will occur in multiple building on the Engineering quad of the University of
-          Illinois at Urbana-Champaign. Specifically, it will mostly occur at the Siebel Center for
-          Computer Science (201 N Goodwin Ave, Urbana, IL 61801) and Electrical &amp; Computer
-          Engineering Building (306 N Wright St, Urbana, IL 61801).
+          HackIllinois will occur in multiple buildings on the University of Illinois at
+          Urbana-Champaign campus, primarily at the Siebel Center for Computer Science (201 N
+          Goodwin Ave, Urbana, IL 61801) and Electrical &amp; Computer Engineering Building (306 N
+          Wright St, Urbana, IL 61801).
         </p>
       </section>
       <section className="faq-column">
         <h4>When</h4>
         <p>
-          Buses should arrive by 5pm on Friday, February 22nd, 2019 for a sponsor fair, opening
-          ceremony, and other activities. The hacking period will begin at 10pm and will last until
-          10am on Sunday, February 24th. A more detailed schedule will be released closer to the
-          event here on our website.
+          Buses will arrive by 5 pm on Friday, February 22 for an opening ceremony and other
+          activities. The contribution period will begin at 10 pm and will last until 10 am on
+          Sunday, February 24. A more detailed schedule will be released closer to the event here on
+          our website.
         </p>
       </section>
       <section className="faq-column">
         <h4>Who</h4>
         <p>
-          All university students of all skill levels are eligible to compete. We’re looking for a
-          diverse mix of universities, disciplines, and interests at our event, and will be
-          providing travel capacity for students not residing in the Urbana-Champaign area.
+          All university and college students of all skill levels are eligible to compete. We
+          welcome a diverse mix of colleges, disciplines, and interests at our event, and will be
+          providing buses to certain locations for students outside the Champaign-Urbana area.
         </p>
       </section>
     </div>
