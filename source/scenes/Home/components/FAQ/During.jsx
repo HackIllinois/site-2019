016f126e56e3893ed41aa5534fdecffbc026f103
Justin Yang <justin@y4ng.com>
2019-01-13 23:22:01 -0600

    Update FAQ (#81)
    * Update FAQ for 2019
    
    * Update applications FAQ
    
    * Removed mention of buses on FAQ.
    
    * Small spelling error fix.
    
    * Line length fix.

--- source/scenes/Home/components/FAQ/During.jsx
+++ source/scenes/Home/components/FAQ/During.jsx
@@ -4,36 +4,34 @@ const During = () => (
   <section className="faq-pane">
     <div className="faq-container faq-column-container">
       <section className="faq-column">
-        <h4>Is there food?</h4>
+        <h4>Will there be food?</h4>
         <p>
-          All meals, snacks, and drinks will be provided for the entire weekend. If you have a
+          Snacks, drinks, and all meals will be provided for the entire weekend. If you have a
           dietary restriction, please make sure to mention it on the application. We’ll have a wide
-          variety of healthy food available throughout the weekend. If you’re at the event and there
-          isn’t food available, let staff know and we’ll fix it immediately.
+          variety of food available throughout the weekend.
         </p>
       </section>
       <section className="faq-column">
         <h4>Who do I work with?</h4>
         <p>
-          Mentors from leading technology companies will be on-hand to provide support and
-          troubleshoot any potential problems. Our team matching process will be announced during
-          opening ceremonies, so don’t be late!
+          Mentors from a variety of open source projects will work with you to learn about open
+          source and make contributions. Project matching will occur primarily over Slack so watch
+          out for details once you have RSVPd.
         </p>
       </section>
       <section className="faq-column">
         <h4>Where do I sleep?</h4>
         <p>
-          There are sleeping rooms furnished with air mattresses and pillows. We want you to be
-          comfortable during the weekend!
+          There will be sleeping rooms furnished with air mattresses and pillows. We want you to be
+          healthy and comfortable during the weekend!
         </p>
       </section>
       <section className="faq-column">
         <h4>Is there anything to do other than code?</h4>
         <p>
-          ABSOLUTELY! We’ll be running a variety of mini-events ranging from our historic
-          Nerf battles to some brand new ideas we will be piloting this year. We’ll have a few
-          keynotes and engaging workshops as well, so definitely familiarize yourself with our
-          schedule before beginning work on your projects.
+          ABSOLUTELY! We’ll be running a variety of mini-events this year, including our customary
+          Nerf battles as well as some new events. We’ll have open source keynotes and engaging
+          workshops as well, so definitely check out the schedule to be posted here on our website.
         </p>
       </section>
     </div>
