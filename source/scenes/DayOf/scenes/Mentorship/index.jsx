690f744a0ac82344e448c126bb58ffc561eb2b41
Jonathan Xu <jonathanj.xu97@gmail.com>
2019-02-10 22:23:35 -0600

    Mentorship page

--- source/scenes/DayOf/scenes/Mentorship/index.jsx
+++ source/scenes/DayOf/scenes/Mentorship/index.jsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
+import mentors from './mentors';
 import './styles.scss';
 
-const Mentorship = () => <h1>Mentorship</h1>;
+type MentorProps = {
+  image: string,
+  name: string,
+  description: string,
+};
+
+const Mentor = (props: MentorProps) => {
+  const { image, name, description } = props;
+
+  return (
+    <div className="mentor-profile">
+      <div className="img-name">
+        <div className="profile-picture">
+          <img src={image} alt={`${name}`} />
+        </div>
+        <h5>{name}</h5>
+      </div>
+      <p>{description}</p>
+      <div className="line" />
+    </div>
+  );
+};
+
+const Mentorship = () => (
+  <Fragment>
+    <h1>Mentorship</h1>
+
+    <div className="mentors">
+      {mentors.map(m => (
+        <Mentor key={m.name} {...m} />
+      ))}
+    </div>
+  </Fragment>
+);
 
 export default Mentorship;
