157dee69fd60647bc3e51aa75e6798fadc850394
kdutka <kdutka2@illinois.edu>
2019-02-14 17:25:49 -0600

    Created Maps for DayOf

--- source/scenes/DayOf/scenes/Maps/index.jsx
+++ source/scenes/DayOf/scenes/Maps/index.jsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import MapECEB from './Maps/HackillinoisMAPS-ECEB.png';
+import MapSIEBEL from './Maps/HackillinoisMAPS-SIEBEL.png';
+import MapKENNY from './Maps/HackillinoisMAPS-KENNY.png';
+import MapDCL from './Maps/HackillinoisMAPS-DCL.png';
 
 import './styles.scss';
 
-const Maps = () => <h1>Maps</h1>;
+const Maps = () => (
+  <div className="flexbox-center">
+    <Tabs>
+      <p className="tab-title">Maps</p>
+
+      <TabList>
+        <Tab>
+          <p className="tab-underline">ECEB</p>
+        </Tab>
+        <Tab>
+          <p className="tab-underline">SIEBEL</p>
+        </Tab>
+        <Tab>
+          <p className="tab-underline">KENNY</p>
+        </Tab>
+        <Tab>
+          <p className="tab-underline">DCL</p>
+        </Tab>
+      </TabList>
+
+      <TabPanel>
+        <div>
+          <img src={MapECEB} alt="" className="bimage" />
+        </div>
+      </TabPanel>
+      <TabPanel>
+        <img src={MapSIEBEL} alt="" className="bimage" />
+      </TabPanel>
+      <TabPanel>
+        <img src={MapKENNY} alt="" className="bimage" />
+      </TabPanel>
+      <TabPanel>
+        <img src={MapDCL} alt="" className="bimage" />
+      </TabPanel>
+    </Tabs>
+  </div>
+);
 
 export default Maps;
---
+++ source/scenes/Maps/index.jsx
@@ -0,0 +1,8 @@
+// @flow
+import React from 'react';
+
+import './styles.scss';
+
+const Home = () => <div className="home">kljsadasdas</div>;
+
+export default Home;
