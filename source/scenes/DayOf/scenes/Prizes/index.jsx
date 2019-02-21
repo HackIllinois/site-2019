990f231cb53087efdcf1d80d506527c166551ce9
Jonathan Xu <jonathanj.xu97@gmail.com>
2019-02-21 14:27:39 -0600

    Prizes page

--- source/scenes/DayOf/scenes/Prizes/index.jsx
+++ source/scenes/DayOf/scenes/Prizes/index.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+// @flow
+import React, { Fragment } from 'react';
 
+import prizes from './prizes.json';
 import './styles.scss';
 
-const Prizes = () => <h1>Prizes</h1>;
+type PrizeProps = {
+  name: String,
+  blurb: String,
+  company: String,
+};
+
+const Prize = (props: PrizeProps) => {
+  const { name, blurb, company } = props;
+  return (
+    <div className="prize">
+      <h2>{name}</h2>
+      <p>
+        <strong>{blurb}</strong>
+      </p>
+      <p>Presented by {company}</p>
+    </div>
+  );
+};
+
+const Prizes = () => (
+  <Fragment>
+    <h1>Prizes</h1>
+
+    {/* Reusing styles from schedule */}
+    <div className="day prizes">
+      {prizes.map(p => (
+        <Prize {...p} key={p.name} />
+      ))}
+    </div>
+  </Fragment>
+);
 
 export default Prizes;
